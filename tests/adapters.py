4a1dc7cacbd403b7aa28c71ae38c7d03eb4a07b0
CyaCoan <cyacoan@163.com>
2026-03-03 20:14:17 +0800

    Implement run_rope adapter

--- tests/adapters.py
+++ tests/adapters.py
@@ -222,7 +222,11 @@ def run_rope(
     Returns:
         Float[Tensor, " ... sequence_length d_k"]: Tensor with RoPEd input.
     """
-    raise NotImplementedError
+    from cs336_basics.nn import RotaryPositionalEmbedding
+
+    layer = RotaryPositionalEmbedding(d_k, theta, max_seq_len, device=in_query_or_key.device)
+
+    return layer(in_query_or_key, token_positions)
 
 
 def run_transformer_block(

92c7a60b6f06eef3d18699678797d829a8866fec
CyaCoan <cyacoan@163.com>
2026-03-12 22:04:19 +0800

    Add file chunking to train_bpe

--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -2,6 +2,54 @@ import os
 from collections import defaultdict, Counter
 import regex as re
 import json
+from typing import BinaryIO
+
+
+def find_chunk_boundaries(
+    file: BinaryIO,
+    desired_num_chunks: int,
+    split_special_token: bytes,
+) -> list[int]:
+    """
+    Chunk the file into parts that can be counted independently.
+    May return fewer chunks if the boundaries end up overlapping.
+    """
+    assert isinstance(split_special_token, bytes), "Must represent special token as a bytestring"
+
+    # Get total file size in bytes
+    file.seek(0, os.SEEK_END)
+    file_size = file.tell()
+    file.seek(0)
+
+    chunk_size = file_size // desired_num_chunks
+
+    # Initial guesses for chunk boundary locations, uniformly spaced
+    # Chunks start on previous index, don't include last index
+    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
+    chunk_boundaries[-1] = file_size
+
+    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+
+    for bi in range(1, len(chunk_boundaries) - 1):
+        initial_position = chunk_boundaries[bi]
+        file.seek(initial_position)  # Start at boundary guess
+        while True:
+            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+
+            # If EOF, this boundary should be at the end of the file
+            if mini_chunk == b"":
+                chunk_boundaries[bi] = file_size
+                break
+
+            # Find the special token in the mini chunk
+            found_at = mini_chunk.find(split_special_token)
+            if found_at != -1:
+                chunk_boundaries[bi] = initial_position + found_at
+                break
+            initial_position += mini_chunk_size
+
+    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
+    return sorted(set(chunk_boundaries))
 
 
 def train_bpe(
@@ -33,38 +81,53 @@ def train_bpe(
     # 计算合并次数（词表大小 = 初始大小 + 合并次数 + 特殊 token 数）
     num_merges = vocab_size - 256 - len(special_tokens)
 
+    total_words_list = []
+    total_counts_list = []
+
     # 打开文件
-    with open(input_path, "r", encoding="utf-8") as f:
-        text = f.read()
+    with open(input_path, "rb") as f:
+        num_processes = 4
+        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
 
-    # 去除特殊 token
-    if special_tokens:
-        special_regex = '|'.join(re.escape(t) for t in special_tokens)
-        parts = re.split(f"{special_regex}", text)
-        train_segments = [p for p in parts if p not in special_tokens]
-    else:
-        train_segments = [text]
+        for start, end in zip(boundaries[:-1], boundaries[1:]):
+            f.seek(start)
+            chunk = f.read(end - start).decode("utf-8", errors="ignore")
+            text = chunk
 
-    # GPT2 的预分词正则表达式
-    gpt2_pat = re.compile(r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
+            # 去除特殊 token
+            if special_tokens:
+                special_regex = '|'.join(re.escape(t) for t in special_tokens)
+                parts = re.split(f"{special_regex}", text)
+                train_segments = [p for p in parts if p not in special_tokens]
+            else:
+                train_segments = [text]
 
-    # 统计预分词后每个单词的频率，将单词转换为字节序列元组作为 Counter 的键
-    raw_counts = Counter()
+            # GPT2 的预分词正则表达式
+            gpt2_pat = re.compile(r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
 
-    for segment in train_segments:
-        words = gpt2_pat.findall(segment)
+            # 统计预分词后每个单词的频率，将单词转换为字节序列元组作为 Counter 的键
+            raw_counts = Counter()
 
-        for word in words:
-            word_tuple = tuple( bytes([b]) for b in word.encode("utf-8") )
-            raw_counts[word_tuple] += 1
+            for segment in train_segments:
+                words = gpt2_pat.findall(segment)
 
-    # 将 Counter 的键和值分别存储在两个列表里
-    words_list = []
-    counts_list = []
+                for word in words:
+                    word_tuple = tuple( bytes([b]) for b in word.encode("utf-8") )
+                    raw_counts[word_tuple] += 1
 
-    for word_tuple, count in raw_counts.items():
-        words_list.append(list(word_tuple))
-        counts_list.append(count)
+            # 将 Counter 的键和值分别存储在两个列表里
+            words_list = []
+            counts_list = []
+
+            for word_tuple, count in raw_counts.items():
+                words_list.append(list(word_tuple))
+                counts_list.append(count)
+
+            total_words_list.extend(words_list)
+            total_counts_list.extend(counts_list)
+
+    words_list = total_words_list
+    counts_list = total_counts_list
 
     # stats: 存储所有可能的相邻字节对 (pair) 及其全局出现频率
     # 结构：{(byte_a, byte_b): frequency}
@@ -83,7 +146,7 @@ def train_bpe(
             indices[pair].add(idx)
 
     merges = []
-
+    
     for i in range(num_merges):
 
         # 如果 stats 为空（所有可能的对都已合并或频率为0），则停止

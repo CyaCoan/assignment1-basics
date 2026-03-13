543ca63233f02d3525c5873229c175b0eed4c1ca
CyaCoan <cyacoan@163.com>
2026-03-13 19:38:07 +0800

    Upload checkpoint.py

---
+++ cs336_basics/checkpoint.py
@@ -0,0 +1,33 @@
+import torch
+import os
+import typing
+
+
+def save_checkpoint(
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    iteration: int,
+    out: typing.Union[str, os.PathLike, typing.BinaryIO, typing.IO[bytes]]
+):
+    
+    checkpoint = {
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'iteration': iteration
+    }
+
+    torch.save(checkpoint, out)
+
+def load_checkpoint(
+    src: typing.Union[str, os.PathLike, typing.BinaryIO, typing.IO[bytes]],
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer
+) -> int:
+    
+    checkpoint = torch.load(src, map_location='cpu')
+
+    model.load_state_dict(checkpoint['model_state_dict'])
+
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+    return checkpoint['iteration']
